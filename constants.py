bb4d4445f51fd26b99a7116d13bdd80b23d00db5
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-05-24 17:53:14 +0530

    Global constants

---
+++ constants.py
@@ -0,0 +1,12 @@
+#For main.py
+FREQ = 44100
+WINDOW_SIZE = 1024
+OVERLAP_RATIO = 0.1
+STORE = "audio.csv"
+OUTPUT = "output.txt"
+SPEC_FOLDER = "specimages/"
+SAVE_IMAGES = True
+
+#For detect.py
+SPEC_FOLDER = "specimages/"
+THRESHOLD = 250
