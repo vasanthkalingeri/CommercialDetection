ff918bb091dcd11e21290c8ce6898c708d5c9ef3
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-05-26 21:44:02 +0530

    Latest work on audio based detection

--- main.py
+++ main.py
@@ -53,14 +53,15 @@ def analyze_spectrogram(filename, wsize=WINDOW_SIZE, fs=FREQ, overlap=OVERLAP_RA
     ]
     
     duration = int(len(channels[0]) / (FREQ * 1.0))
+    print duration
     time_splits = []
-    for i in xrange(duration):
-        start = i*(FREQ)
+    for i in xrange(duration / 2):
+        start = i*(FREQ)*2
         
         # generate specgram
         pylab.axis("off")
         Pxx, freqs, t, plot = pylab.specgram(
-            channels[0][start:(FREQ) + start],
+            channels[0][start:(FREQ*2) + start],
             NFFT=wsize, 
             Fs=fs*20, 
             detrend=pylab.detrend_none,
@@ -76,9 +77,10 @@ def analyze_spectrogram(filename, wsize=WINDOW_SIZE, fs=FREQ, overlap=OVERLAP_RA
         img = cv2.imread(TEMP_FILE)
         os.remove(TEMP_FILE)
         if detect.analyze(img):
-            time_splits.append(i)
+            print "Found a split at", get_time_string(i * 2)
+            time_splits.append(i * 2)
         sys.stdout.write('\r')
-        sys.stdout.write("[%-20s] %d%%" % ('='*i, int(i / duration)))
+        sys.stdout.write("[%-20s] %d%%" % ('='*(i / (2 * duration)), i))
         sys.stdout.flush()
     
     return time_splits
@@ -107,7 +109,7 @@ def generate_audio(filename):
     
 def main():
     
-    generate_audio(sys.argv[1])
+#    generate_audio(sys.argv[1])
     print "Analyzing audio"
     time_splits = analyze_spectrogram(AUDIO_FILE)  
     write_output(time_splits) 
