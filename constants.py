51a734bd703b567cc7baeff2663478e29b48b3d4
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-05-29 01:58:07 +0530

    updated

--- constants.py
+++ constants.py
@@ -6,10 +6,16 @@ STORE = "audio.csv"
 OUTPUT = "output.txt"
 SPEC_FOLDER = "specimages/"
 SAVE_IMAGES = False
-AUDIO_FILE = "temp.wav"
+AUDIO_FILE = "aud.wav"
 TEMP_FILE = "temp.png"
 TIME_THRESH = 20
 
 #For detect.py
 SPEC_FOLDER = "specimages/"
 THRESHOLD = 300
+
+#For generate.py
+DB_FOLDER = "db/"
+DB_AUDIO = DB_FOLDER + "audio/"
+DB_VIDEO = DB_FOLDER + "video/"
+DBNAME = "commercials.csv"
