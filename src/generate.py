e0b65b8f59f2947aa20ec17342d7aa8848ba4abc
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-06-15 12:11:41 +0530

    Working but slow

---
+++ src/generate.py
@@ -0,0 +1,88 @@
+import os
+from constants import *
+from dejavu import Dejavu
+from dejavu.recognize import FileRecognizer
+import timeFunc
+import ffmpeg
+
+class Generate(object):
+
+    def __init__(self, labels, video_name):
+    
+        self.label_name = labels
+        self.video_name = video_name
+        self.djv = Dejavu(CONFIG)
+    
+    def read_lables(self):
+        
+        with open(self.label_name) as fd:
+            for line in fd:
+                line = line.split("=")
+                line = [i.strip() for i in line]
+                time = line[0].split("-")
+                time = [i.strip(" ") for i in time]
+                delta_str = timeFunc.get_delta_string(time[0], time[1])
+                name = line[-1].strip()
+                yield [time[0], name, delta_str]
+
+    def build_db(self, aud_ext=".wav", vid_ext=".mpg"):
+        
+        labels = self.read_lables()
+        
+        filename = len(os.listdir(DB_AUDIO)) + 1 #Number of files in the directory + 1
+        if filename == 1:
+        
+            #Creating for the first time
+            f = open(DBNAME, "w")
+            f.write("name, duration, path, verified")
+        
+        else:
+        
+            #Already exists
+            f = open(DBNAME, "a")
+                
+        for data in labels:
+        
+            start = data[0]
+            name = data[1]
+            duration = data[2]
+            
+            #Create a file in the db folder, audio and video are stored seperately 
+            ffmpeg.create_video(start, duration, self.video_name, DB_VIDEO + str(filename) + vid_ext)
+            ffmpeg.create_audio(DB_VIDEO + str(filename) + vid_ext, DB_AUDIO + str(filename) + aud_ext)
+            
+            #Create a corresponding entry in the csv file
+            s = ",".join(data[1:])
+            s = s + "," + DB_VIDEO + str(filename) + vid_ext + ",yes\n" #Check verified to be true since human tagged
+            f.write(s)
+            filename += 1
+
+    def fingerprint_db(self, aud_ext=".wav", no_proc=1):
+        
+        self.djv.fingerprint_directory(DB_AUDIO, [aud_ext])
+        
+    def clean_db(self, aud_ext=".wav", vid_ext=".mpg"):
+        
+        choice = raw_input("Are you sure you want to remove all commercials in the database? (yes/no)")
+        if choice == "yes":
+            print "Cleaning database.."
+            filename = len(os.listdir(DB_AUDIO)) + 1
+            for i in range(filename):
+                os.remove(DB_AUDIO + str(i) + aud_ext)
+                os.remove(DB_VIDEO + str(i) + vid_ext)
+                os.remove(DBNAME)
+            print "Database is empty"
+        
+    def run(self, aud_ext=".wav", vid_ext=".mpg"):
+        
+        self.build_db(aud_ext, vid_ext)
+        self.fingerprint_db(aud_ext, vid_ext)
+        
+def test():
+    
+    gen = Generate("../data/labels_2015-04-28_0000_US_KABC", "../data/2015-04-28_0000_US_KABC_Eyewitness_News_5PM.mpg")
+    gen.run()
+    gen = Generate("../data/labels", "../data/test.mpg") 
+    gen.run()
+
+test()
--- src/recognize.py
+++ src/recognize.py
@@ -1,11 +1,10 @@
-from dejavu.recognize import DataRecognizer, FileRecognizer
+from dejavu.recognize import FileRecognizer
 import os
 import subprocess
 import timeFunc
 from constants import *
 import ffmpeg
-from dejavu import Dejavu, decoder
-from scipy.io import wavfile
+from dejavu import Dejavu
 
 class Recognize(object):
     
@@ -13,12 +12,20 @@ class Recognize(object):
         
         self.video_name = video_name
         self.djv = Dejavu(CONFIG)
+
+    def get_vid_length(self):
         
-        #We create the audio for the video, extract the frames and delete the audio created
-        ffmpeg.create_audio(self.video_name, TEMP_AUDIO)
-        self.frames, self.Fs, file_hash = decoder.read(self.video_name)
-        self.Fs = 44100
-#        self.clean()
+        filename = self.video_name
+        result = subprocess.Popen(["ffprobe", filename],
+        stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+        l = [x for x in result.stdout.readlines() if "Duration" in x]
+        
+        #Some clipping to obtain the time string, this is specific to the version of ffmpeg/ffprobe
+        l = l[0].split(",")
+        time = l[0]
+        time = time.split(" ")[-1]
+        time = time.split('.')[0] #Remove milliseconds
+        return time
 
     def get_line(self, filename, index):
         
@@ -34,29 +41,25 @@ class Recognize(object):
         
         #removes the temp files that were created
         os.remove(TEMP_AUDIO)
-#        os.remove(TEMP_VIDEO)
+        os.remove(TEMP_VIDEO)
             
-    def recognize_audio(self, start, span=VIDEO_SPAN):
-        
-        """Recognize the audio between start and end
-           start = duration in seconds from the start of the video(Use timeFunc.get_seconds() if in h:m:s format)
-        """
+    def recognize_audio(self, start):
         
         print timeFunc.get_time_string(start), "COMPLETE"
-        song = self.djv.recognize(DataRecognizer, self.frames[0][start*self.Fs:(start+span)*self.Fs])
-#        song = self.djv.recognize(FileRecognizer, self.video_name)
-        print song
+        song = self.djv.recognize(FileRecognizer, TEMP_AUDIO)
+        self.clean()
+        if song is None:
+            return VIDEO_GAP
         if song[DJV_CONFIDENCE] >= CONFIDENCE_THRESH:
             #obtain the start of the commercial
             start -= song[DJV_OFFSET]
             start = int(start)
             
             #Read the database to obtain the end time of the commercial
-            index = int(song[DJV_SONG_NAME]) #This is the line containing the db details
+            index = int(song[DJV_SONG_NAME]) - 1 #This is the line containing the db details
             line = self.get_line(DBNAME, index)
             line = line.split(",")
             print line
-            
             name = line[0]
             duration = line[1]
             duration = timeFunc.get_seconds(duration)
@@ -69,30 +72,32 @@ class Recognize(object):
             f = open(OUTPUT, "a")
             s = timeFunc.get_time_string(start) + " - " + timeFunc.get_time_string(end) + " = " + name 
             if verified == DB_UNCLASSIFIED: #Has not been verified in the database
-                s += DB_UNCLASSIFIED
+                s += " " + DB_UNCLASSIFIED
             else:
-                s += DB_CLASSIFIED
+                s += " " + DB_CLASSIFIED
             s += "\n"
             f.write(s)
             f.close()
             return (duration + 1)
         else:
             return VIDEO_GAP
-    
+            
     def recognize(self):
         
-        #First we obtain the audio of the entire video
-        duration = len(self.frames[0]) / (self.Fs*1.0)
-        duration = int(duration)
-        print duration, self.Fs, len(self.frames)
+        #Generates temp.mpg which is the temp video file and temp.wav, its corresponding audio file
+        
+        duration = self.get_vid_length()
+        duration = timeFunc.get_seconds(duration)
         base = 0
         while base < duration:
-            print base
+            ffmpeg.create_video(timeFunc.get_time_string(base), timeFunc.get_time_string(VIDEO_SPAN), self.video_name, TEMP_VIDEO)
+            ffmpeg.create_audio(TEMP_VIDEO, TEMP_AUDIO)
             base += self.recognize_audio(base)
     
 def test():
     
-    recog = Recognize("../data/shottest.mpg")
+    recog = Recognize("../data/shot_det.mpg")
     recog.recognize()
 
 test()
+
