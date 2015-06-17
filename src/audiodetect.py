9d0362da1a0820eab2eee3a01c9f516f1fa9d6c1
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-06-18 02:07:05 +0530

    Initializing np array, works faster

--- src/audiodetect.py
+++ src/audiodetect.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.io import wavfile
 import timeFunc
-import hickle
+#import hickle
 from constants import *
 
 class DetectSilence(object):
@@ -9,10 +9,12 @@ class DetectSilence(object):
     def __init__(self, audio_name):
         
         self.audio_name = audio_name
-        self.X = np.array([])
         self.Fs = None
-                
-    def get_data(self, window=WINDOW_SIZE, overlap=0):
+        self.times = []
+        self.times_dic = {}
+        self.X = None
+        
+    def get_data(self, window, overlap):
 
         """
             Gets the freq region of data
@@ -20,29 +22,37 @@ class DetectSilence(object):
             Window = size of the window in seconds, by default it creates a window of 2 milliseconds
             Overlap = ratio of overlap between frames
         """
+        
         self.Fs, frames = wavfile.read(self.audio_name)
         start = 0
         i = window
-        m = 0
+        m = int(len(frames) / (window * self.Fs) - 1) # As (k + 1)*window*fs < len(frames)
+        
+        #Creating an empty X matrix
+        n = np.fft.fftfreq(int(self.Fs*window))[: self.Fs // 2].shape[0] #Only real part
+        n = n / 2 + 1 #Since we take only half of real part
+        self.X = np.zeros((m, n), dtype=np.float32)
+        
+        k = 0
         while ((i*self.Fs) < len(frames)):
+        
             print timeFunc.get_time_string(i)
             end = start + int(self.Fs * window)
-            x = np.array(frames[start:end]) + 0.0001#To remove any zero errors
+            x = np.array(frames[start:end], dtype=np.float32) + 0.0000001#To remove any zero errors
             print x.shape, start, end
             print end-start
-            magnitudes = np.abs(np.fft.rfft(x))[:self.Fs / 4] #Only these are used for similarity measure
-            self.X = np.append(self.X, magnitudes)
+            magnitudes = np.abs(np.fft.rfft(x))[:self.Fs / 4]
+            self.X[k] = np.copy(magnitudes)
             start += int(self.Fs * (1 - overlap) * window)
             i += window
-            m += 1
+            k += 1
+        
         print self.X.shape
-        print m, self.Fs/4
-        self.X = np.reshape(self.X, (m, len(magnitudes)))
 #        hickle.dump(self.X, '../data/newdat.hkl', mode='w')
 
     def detect(self):
         
-        #Each step of X is 10ms
+#        self.X = hickle.load('../data/newdat.hkl')
         m,n= self.X.shape
         print m, n
         
@@ -50,31 +60,36 @@ class DetectSilence(object):
         times = []
         for i in range(m):
             magnitudes = self.X[i, :]
-            val = (np.min(magnitudes) + np.max(magnitudes)) / np.var(magnitudes)
+            val = (np.max(magnitudes) + np.min(magnitudes)) / (np.var(magnitudes))
             val *= 100
             print timeFunc.get_time_string(i * WINDOW_SIZE), val
             if val > 1:
-                times.append(timeFunc.get_time_string(i * WINDOW_SIZE))
+                ts = timeFunc.get_time_string(i * WINDOW_SIZE)
+                try:
+                    self.times_dic[ts] += 1
+                except:
+                    self.times_dic[ts] = 0
 
         j = 0
-        while j < len(times) - 1:
-            delta = timeFunc.get_delta_string(times[j], times[j + 1])
-            delta = timeFunc.get_seconds(delta)
-            if delta <= 1:
-                times.remove(times[j])
-            else:
-                j += 1
-        print times
+        times = self.times_dic.keys()
+        times.sort
+        for time in times:
+            if self.times_dic[time] == 0: #Occurred only once, very low chances of it being valid
+                del self.times_dic[time]
+        self.times = self.times_dic.keys()
+        print self.times_dic
+        self.times.sort()
     
-    def run(self):
+    def get_times(self, window=WINDOW_SIZE, overlap=OVERLAP):
         
-        self.get_data()
+        self.get_data(window, overlap)
         self.detect()
-            
-def test():
-    
-    print "running"
-    DetectSilence("../data/aud_s.wav").run()
-    print "Done with all"
-    
-test()
+        return self.times
+#            
+#def test():
+#    
+#    print "running"
+#    print DetectSilence(TEMP_AUDIO).get_times()
+#    print "Done with all"
+#    
+#test()
