682e2c5a086160ec767195cf75ee70817f7b9469
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-06-18 00:59:07 +0530

    Giving correct output

--- src/recognize.py
+++ src/recognize.py
@@ -7,6 +7,8 @@ import ffmpeg
 from dejavu import Dejavu, decoder
 import numpy as np
 from audiodetect import DetectSilence
+from fileHandler import LabelsFile, DatabaseFile
+
 
 class Recognize(object):
     
@@ -25,31 +27,19 @@ class Recognize(object):
         self.frames, self.Fs, hash_val = decoder.read(TEMP_AUDIO)
         self.frames = self.frames[0] #Since we always create single channel audio
         self.duration = int(self.frames.shape[0] / (self.Fs*1.0)) #Duration of the entire video in seconds
-        
-    def get_line(self, filename, index):
-        
-        f = open(filename)
-        i = 0
-        for line in f:
-            if i == index:
-                return line
-            i += 1
-        return -1
 
     def find_commercial(self, start, span=VIDEO_SPAN):
         
-        print timeFunc.get_time_string(start), "COMPLETE"
+#        print timeFunc.get_time_string(start), "....."
         data = np.copy(self.frames[start*self.Fs:(start+span)*self.Fs])
-        print timeFunc.get_time_string(start+span)
         song = self.djv.recognize(DataRecognizer, [data])
         if song is None:
-            return [start + VIDEO_SPAN, False]
-        print song
+            return [start, []] #We can safely skip
         if song[DJV_CONFIDENCE] >= CONFIDENCE_THRESH:
 
             if song[DJV_OFFSET] < 0:
                 #Offset can never be greater than duration of detected commercial
-                return [start+VIDEO_SPAN, False]
+                return [start, []]
                             
             #obtain the start of the commercial
             start -= song[DJV_OFFSET]
@@ -57,48 +47,62 @@ class Recognize(object):
             
             #Read the database to obtain the end time of the commercial
             index = int(song[DJV_SONG_NAME]) #This is the line containing the db details
-            line = self.get_line(DBNAME, index)
-            line = line.split(",")
-            name = line[0]
-            duration = line[1]
-            duration = timeFunc.get_seconds(duration)
+            name, duration, verified = DatabaseFile(DBNAME).get_line(index)
             
             if duration < song[DJV_OFFSET]:
                 #The offset where it is found cannot exceed the duration of the song
-                return [start+VIDEO_SPAN, False]
+                return [start, []]
             
-            verified = line[-1]
             end = start + duration
-                
-            f = open(OUTPUT, "a")
-            s = timeFunc.get_time_string(start) + " - " + timeFunc.get_time_string(end) + " = " + name 
-#            if verified == DB_UNCLASSIFIED: #Has not been verified in the database
-#                s += " " + DB_UNCLASSIFIED
-#            else:
-#                s += " " + DB_CLASSIFIED
-            s += "\n"
-            f.write(s)
-            f.close()
-            return [end, True]
+                      
+            return [end, [start, name]]
         else:
-            return [start+VIDEO_SPAN, False]
+            return [start, []]
             
     def recognize(self):
         
         #Generates temp.mpg which is the temp video file and temp.wav, its corresponding audio file
         
-        times = DetectSilence(TEMP_AUDIO).get_times()
-        print times
+#        times = DetectSilence(TEMP_AUDIO).get_times()
+#        print times
+#        assert False
+        
+        times =['00:00:19', '00:00:20', '00:00:21', '00:00:22', '00:00:23', '00:00:24', '00:00:25', '00:00:26', '00:00:27', '00:00:28', '00:00:29', '00:00:30', '00:00:31', '00:00:32', '00:00:33', '00:00:34', '00:00:35', '00:00:36', '00:00:37', '00:00:38', '00:00:39', '00:00:40', '00:00:41', '00:00:42', '00:00:43', '00:00:44', '00:00:45', '00:00:46', '00:00:47', '00:00:48', '00:00:49', '00:01:19', '00:01:49', '00:01:50', '00:01:51', '00:01:52', '00:01:53', '00:01:54', '00:02:24', '00:02:25', '00:03:03']
+        
 #        times = ['00:00:19', '00:00:20', '00:00:21', '00:00:22', '00:00:23', '00:00:24', '00:00:25', '00:00:26', '00:00:27', '00:00:28', '00:00:29', '00:00:30', '00:00:31', '00:00:32', '00:00:33', '00:00:34', '00:00:35', '00:00:36', '00:00:37', '00:00:38', '00:00:39', '00:00:40', '00:00:41', '00:00:42', '00:00:43', '00:00:44', '00:00:45', '00:00:46', '00:00:47', '00:00:48', '00:00:49', '00:01:19', '00:01:49', '00:01:50', '00:01:51', '00:01:52', '00:01:53', '00:01:54', '00:02:24', '00:02:25', '00:02:38', '00:02:41', '00:03:03', '00:03:13', '00:03:21']#For shottest.mpg
+
         times = [timeFunc.get_seconds(i) for i in times]
+        times.append(self.duration)
         i = 0
         end = 0
+        name = UNCLASSIFIED_CONTENT
+        silence = False
+        sil_start = 0
+        print timeFunc.get_time_string(0), timeFunc.get_time_string(times[0]), UNCLASSIFIED_CONTENT
         while i < len(times) - 1:
-            if end < times[i]:
-                div = (times[i] + times[i + 1]) / 2
+            div = (times[i] + times[i + 1]) / 2 #This eliminates errors due to fade in/out of commercials
+            if times[i + 1] - times[i] > 1:
+                #If not consequtive blocks of silence
+
+                if silence is True:#Was silent before
+                    print timeFunc.get_time_string(sil_start), timeFunc.get_time_string(times[i]), SILENCE
+                    silence = False                  
+                
                 end, res = self.find_commercial(div)
+                if len(res) != 0:#Ad was detected
+                    start = res[0]
+                    name = res[1]
+                else:
+                    start = times[i]
+                    name = UNCLASSIFIED_CONTENT
+    #                LabelsFile().write_labels(TEMP_FILE, [start, end, name])
+                print timeFunc.get_time_string(times[i]), timeFunc.get_time_string(times[i+1]), name
+            else:
+                #Consequtive blocks of silence
+                if silence is False:
+                    sil_start = times[i]
+                    silence = True
             i += 1
-            
                 
 #    def __del__(self):
 #        
