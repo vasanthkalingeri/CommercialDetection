653a1a36798f27fc76a57d82bf1163e8f181a4ce
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-06-22 12:22:16 +0530

    Added video converter for html5 support

--- src/ffmpeg.py
+++ src/ffmpeg.py
@@ -11,15 +11,20 @@ def create_video(start, duration, video_src, video_dst, force_fps=False, fps=60)
     print "Video created!!"
     print
     
-def create_audio(video_src, audio_dst, rate="44100", channels="1", block="160k"):
+def create_audio(video_src, audio_dst, rate=44100, channels=1, block="160k"):
     
     print
     print "Creating audio for the video"
 #    print "ffmpeg -i " + video_src + " -ab " + block + " -ac " + channels + " -ar " + rate + " -vn " + audio_dst
-    os.system("ffmpeg -i " + video_src + " -ab " + block + " -ac " + channels + " -ar " + rate + " -vn " + audio_dst + " -loglevel quiet")
+    os.system("ffmpeg -i " + video_src + " -ab " + block + " -ac " + str(channels) + " -ar " + str(rate) + " -vn " + audio_dst + " -loglevel quiet")
     print "Audio created!!"
     print
     
 def create_images(video_name, fps, folder):
 
     os.system("ffmpeg -i " + video_name + " -vf fps=1/" + str(fps) + " " + folder + "%d.png")
+    
+def convert_video(video_name, cpu_cores=2, preset="superfast"):
+    
+    name, extension = video_name.split('.')
+    os.system("ffmpeg -i " + video_name + " -r 5 -cpu-used " + str(cpu_cores) + " " + name + ".webm -preset " + preset)
