bee6e64823012010bf8ac5da5fbd6e37fc4489e5
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-06-27 03:57:02 +0530

    updated

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from recognize import Recognize
+from generate import Generate
 import sys
+import ffmpeg
+import os
+from constants import *
 
-if sys.argv[1] == "-recognize":
+if sys.argv[1] == "-r":
     recog = Recognize(sys.argv[2])
     recog.recognize()
 
-elif sys.argv[1] == "-generate":
+elif sys.argv[1] == "-g":
     gen = Generate(sys.argv[2], sys.argv[3])
     gen.run()
+
+elif sys.argv[1] == "-l":
+
+    global WEB_VIDEO_NAME
+    video_name = sys.argv[2]
+    ffmpeg.convert_video(video_name, "web/ouput/static")    
+    name, extension = video_name[-5:].split('.')
+    WEB_VIDEO_NAME = name
+    os.system('python web/manage.py runserver')
+    
