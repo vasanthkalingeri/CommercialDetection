016817a7d4bdf71f1e08d11faf80f24586cea0c7
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-08-04 22:50:20 +0530

    Made the skeleton for web interface

--- src/web/output/static/output/output.js
+++ src/web/output/static/output/output.js
@@ -2,8 +2,62 @@ function play(time)
 {
 	var myPlayer = videojs('mainvid');
 	videojs("mainvid").ready(function(){
-          var myPlayer = this;
+//          alert(myPlayer);
+//          alert(time);
           // EXAMPLE: Start playing the video.
+          myPlayer.play();
           myPlayer.currentTime(time);
+          myPlayer.play();
     });
+}
+
+function get_secs(time_str)
+{
+    var times = time_str.split(':');
+    var value = 0;
+    for (var i in times)
+        value += parseInt(times[i]) * Math.pow(60, 2 - i);     
+    return value;
+}
+
+function get_str(secs)
+{
+    var str = "";
+    
+    str = round(secs / 3600) + ":"; 
+}
+
+function add(start, end, id)
+{
+//    var new_end = prompt("Keep till?", end);
+    var index = id.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(index + 1);
+    var cells = [];
+    for (i=0;i<6;i++)
+        cells.push(row.insertCell(i));
+    var start = '{{item.0}}<input type="hidden" name="start{{item.3}}" value="{{item.0}}"></input>';
+    var end = '{{item.1}}<input type="hidden" name="end{{item.3}}" value="{{item.1}}"></input>';
+    var name = '<input type="text" name="name{{item.3}}" value="{{item.2}}"></input>';
+    var play = '<button type="button" onClick="play({{item.3}});">Seek</button>';
+    var add = '<button type="button" onClick="add({{item.3}},{{item.4}}, this);">+</button>';
+    var remove = '<button type="button" onClick="delete({{item.3}},{{item.4}}, this);">-</button>';
+    var data = [start, end, name, play, add, remove];
+    
+    for (i=0;i<6;i++)
+        cells[i].innerHTML = data[i];
+
+//    if(get_secs(new_end) > get_secs(end))
+//    {
+//        alert("Invalid range to split!!");
+//        return;
+//    }
+}
+
+function drop(start, end, id)
+{
+    var index = id.parentNode.parentNode.rowIndex;
+    var choice = prompt("Do you really want to delete this?(y/n)");
+    if(choice == 'y')
+        document.getElementById("myTable").deleteRow(index);
 }
