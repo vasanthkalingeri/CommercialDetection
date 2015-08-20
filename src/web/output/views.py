0cb18b13d4254798a1ac0797fc3eac98d0b4c584
vasanth kalingeri <vasanth.kalingeri@gmail.com>
2015-08-21 00:35:36 +0530

    Added code documentation

--- src/web/output/views.py
+++ src/web/output/views.py
@@ -12,52 +12,98 @@ import fileHandler
 import timeFunc
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import simplejson
-#from generate import Generate
 
 lines = {} #dictionary, with key as start_secs
 
 def get_dict(labels):
     
-#    Each element of list is
-#        [start, end, name, start_secs, end_secs]
+    """
+        Given labels(output from the labels object in fileHandler.py), it creates a dictionary of the form {key:value}
+        Where,
+        key = start time in seconds = start_secs
+        value = line corresponding to that start time = [start, end, name, start_secs, end_secs]
+        Returns: Dictionary
+    """
+    
     d = {}
     for item in labels:
+        #Things are extracted based on labels format
         name = item[2]
+        
+        #We obtain the start and end time in seconds
         start_secs = timeFunc.get_seconds(item[0])
         end_secs = timeFunc.get_seconds(item[1])
+        
         item.append(start_secs)
         item.append(end_secs)
+        
+        #Create the dictionary
         d[start_secs] = item
+        
     return d
 
 @csrf_exempt
 def index(request):
     
+    """
+        The url /output maps to this function.
+        Displays the video and the corresponding labels file on the browser.
+        This page is called in two ways:
+            normal way: When the user visits the site
+            Ajax way: When user makes some changes to the labels, the page is reloaded with the help of this function
+    """
+    
     global lines
     t = get_template('output/index.html')
+    
+    #If the page requested by the user(and not ajax), we have to read the labels file
     if request.is_ajax() == False:
+        #Page was requested by the user
         labels = fileHandler.LabelsFile(infile=BASE_DIR + "/../" + WEB_LABELS).read_lables(skip=False)
         lines = get_dict(labels)
+    
+    #Since keys are assorted in a dict, we sort them.
     keys = lines.keys()
     keys.sort()
+    
+    #Now we have start_secs in increasing order, store this value in values.
     values = [lines[key] for key in keys]
+    
     html = t.render(Context({'video_path': WEB_VIDEO_NAME, 'item_list': values}))
     return HttpResponse(html)
 
 @csrf_exempt
 def update(request):
     
+    """
+        The url /output/update is mapped to this function.
+        This function is always called through ajax
+        When the user edits any label on the browser, this function is called to reflect the changes in "lines" dictionary
+    """
+    
     global lines 
+    
+    #Obtain the start_secs of the label which the user just edited
     start = int(request.POST.get(u'start'))
+    
+    #Obtain the new text
     text = str(request.POST.get(u'text'))
-    #Now we update the value in lines as well
+    
+    #Update the text
     l = lines[start]
     l[2] = text
     lines.update({start:l})
+    
     return HttpResponse(simplejson.dumps({'server_response': '1' }))
 
 @csrf_exempt
 def save(request):
+    
+    """
+        The url /output/save/ is mapped to this function.
+        This function is called with the click of the "Save changes" button.
+        The function writes the "lines" dictionary back into the labels file and ends the program.
+    """
     
     global lines
     labels = fileHandler.LabelsFile(outfile=BASE_DIR + "/../" + WEB_LABELS)
@@ -72,17 +118,30 @@ def save(request):
 @csrf_exempt
 def delete(request):
     
+    """
+        The url /output/delete/ maps to this function.
+        This function is called by the click of the button '-'.
+        It is used to delete the label, intended to be deleted by the user.
+        When a label is deleted the following operations take place:
+            - the end time of the to be deleted label is written onto the end time of the label preceeding it.
+            - the label to be deleted is removed from lines dictionary
+    """
+    
     global lines
     keys = lines.keys()
     keys.sort()
     start = int(request.POST.get(u'start_sec'))
     end = int(request.POST.get(u'end_sec'))
     
-    #Now we find the key which had the previous start
+    #Now we find the preceeding label
     for i in range(len(keys)):
         if keys[i] == start:
             break
+    
+    #This will be the label, just above the label to be deleted
     old_start = keys[i - 1]
+    
+    #Performing the operations
     
     #We assign the endtime of this to the previous start
     lines[old_start][1] = timeFunc.get_time_string(end)
@@ -90,11 +149,20 @@ def delete(request):
     
     del lines[start]
     
-    print lines[old_start]
     return HttpResponse(simplejson.dumps({'server_response': '1' }))
 
 @csrf_exempt    
 def add(request):
+    
+    """
+        The url /output/add/ maps to this function.
+        The function is called by the click of the button '+'.
+        It is used to add another label.
+        When the function is called, these are the following operations performed.
+            - Obtain the new start time in seconds of the next label
+            - Make the end time of the new label, equal to the end time of the original label(where + was clicked)
+            - Change the end time of the previous label(the label whose + was clicked) to the new start time
+    """
     
     global lines 
     actual_start = int(request.POST.get(u'actual_start'))
